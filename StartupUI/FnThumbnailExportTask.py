e1d8ae23e4ca8ac603635123a4925633bc899ca3
antiero <ant@chainssd.local>
2013-10-12 17:40:08 +0100

    Updating Thumbnail exporter to work with Shots
    Still need to fix bug with colourspace being incorrect

--- StartupUI/FnThumbnailExportTask.py
+++ StartupUI/FnThumbnailExportTask.py
@@ -3,9 +3,12 @@
 import os.path
 import os
 import sys
-
+import hiero.core
 from hiero.core import *
 
+def mapRetime(ti, timelineTime):
+  """Maps the trackItem source in time to timeline in time, handling any retimes"""
+  return ti.sourceIn() + int((timelineTime - ti.timelineIn()) * ti.playbackSpeed())
 
 class ThumbnailExportTask(TaskBase):
   def __init__( self, initDict ):
@@ -93,7 +96,10 @@ class ThumbnailExportTask(TaskBase):
      
   def taskStep(self):
     # Write out the thumbnail for each item
-    if isinstance(self._item, (Sequence, Clip)):
+    if isinstance(self._item, (Sequence, Clip, TrackItem)):
+
+        # Get the frame type
+        frameType = self._preset.properties()['frameType']
         self._thumbFile = self.resolvedExportPath()
         print 'RESOLVED EXPORT PATH IS: ' + str(self._thumbFile)
         
@@ -104,29 +110,47 @@ class ThumbnailExportTask(TaskBase):
           start, end = self.sequenceInOutPoints(self._item, 0, self._item.duration() - 1)
 
           print "SEQUENCE START, END is: %i, %i" % (int(start), int(end))
-
-          # Get the frame type
-          frameType = self._preset.properties()['frameType']
           if frameType == self.kFirstFrame:
             thumb = self._item.thumbnail(start)
           if frameType == self.kMiddleFrame:
             thumb = self._item.thumbnail(start+((end-start)/2))
           if frameType == self.kLastFrame:
             thumb = self._item.thumbnail(end)
+        
+        elif isinstance(self._item, TrackItem):
+          if frameType == self.kFirstFrame:
+            FreezeFrame = self._item.sourceIn()
+          elif frameType == self.kMiddleFrame:
+            FreezeFrame = int((self._item.sourceOut()-self._item.sourceIn())/2)
+          elif frameType == self.kLastFrame:
+            FreezeFrame = self._item.sourceOut()
 
+          # The Colour Transform of the TrackItem
+          colourTransform = self._item.sourceMediaColourTransform()
+
+          # This is the (convoluted) magic to create a single Frame Clip in the Bin
+          fileKnob,currentFrame,ti = self.getFreezeFrameInfoFromTrackItemAtTime(self._item,FreezeFrame)
+
+          # This is an adjustment for the Timeline time of the current TrackItem
+          FreezeFrame = currentFrame+self._item.timelineIn()
+
+          # This creates a single Frame object
+          FreezeFrameClip = hiero.core.Clip(hiero.core.MediaSource(fileKnob),FreezeFrame, FreezeFrame)
+
+          thumb = FreezeFrameClip.thumbnail()
+          # This adds the FreezeFrameClip to the Bin and returns it with the appropriate TrackItem colourTransform applied
+
+        try:
           print 'SAVING %s TO: self._thumbFile' % str(thumb)
 
           thumbSize = self._preset.properties()['thumbSize']
           if thumbSize != "Default":
             thumb = thumb.scaledToHeight(int(thumbSize))
+
           thumb.save(self._thumbFile)
-        
-        elif isinstance(self._item, TrackItem):
-          handles = self._cutHandles if self._cutHandles is not None else 0
-          start, end = (self._item.timelineIn() - handles), (self._item.timelineOut() + handles) + 1
-          # If trackitem write out just the audio within the cut
-          self._sequence.writeAudioToFile(self._audioFile, start, end)
-        
+        except:
+          print 'Unable to save thumb'
+  
     self._finished = True
     
     return False
