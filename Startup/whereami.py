02f42c9a7b09516cf9de340548d81fccce2de62e
antiero <ant@chainssd.local>
2014-02-10 21:47:02 +0000

    Updated to include 'Version Everywhere' action for Clips
    This should be split out into separate methods for Versioning specific
    menu. Still requires handling of Multiple Clips and Bins.

--- Startup/whereami.py
+++ Startup/whereami.py
@@ -1,4 +1,5 @@
 import hiero.core
+from PySide.QtGui import *
 
 def whereAmI(self, searchType='TrackItem'):
   """returns a list of objects which contain this Clip. 
@@ -45,3 +46,95 @@ def whereAmI(self, searchType='TrackItem'):
 
 # Add new method to Clip object
 hiero.core.Clip.whereAmI = whereAmI
+
+# Menu which adds a Tags Menu to the Viewer, Project Bin and Timeline/Spreadsheet
+class VersionAllMenu:
+
+  def __init__(self):
+      self._versionEverywhereMenu = None
+      self._versionActions = []
+
+      hiero.core.events.registerInterest("kShowContextMenu/kBin", self.binViewEventHandler)
+
+  def makeActionFromVersion(self, version):
+    action = QAction(version.name(),None)
+    action.setData(lambda: version)
+
+    # We do this magic, so that the title string from the action is used to set the frame rate!
+    def methodWrapper():
+      print 'METHOD WRAPPER CALLED'
+      currentClip = version.item()
+      trackItems = currentClip.whereAmI()
+      for ti in trackItems:
+        print 'Setting TrackItem %s to be this version: %s ' % (str(ti), str(version))
+        ti.setCurrentVersion(version)
+    
+    action.triggered.connect( methodWrapper )
+    return action  
+
+  # This generates the Version Up Everywhere menu
+  def createVersionEveryWhereMenuForView(self, viewName):
+
+    selection = hiero.ui.activeView().selection()
+    self._versionActions = []
+    if len(selection)==0:
+      return
+
+    versionEverywhereMenu = QMenu("Version Up Everywhere")
+
+    # If we're in the Bin, we could have a Sequence, Bin(s), or Clip(s) selected.
+    # We must ignore the Sequence case, and treat the other cases.
+    if viewName == 'kBin':
+      # If we've selected one Clip, we'll present a unique list of all available versions to pick form
+
+      clipBinItems = [item for item in selection if isinstance(item.activeItem(),hiero.core.Clip)]
+
+      print 'Got this many Clips: ' + str(clipBinItems)
+      bins = [item for item in selection if isinstance(item,hiero.core.Bin)]
+
+      # We search inside of a Bin for a Clip which is not already in clipBinItems
+      if len(bins)>0:
+        # Grab the Clips inside of a Bin and append them to a list
+        for bin in bins:
+          clips = hiero.core.findItemsInBin(bin,'Clip')
+          for clip in clips:
+            if clip not in clipBinItems:
+              clipBinItems.append(clip)
+
+      # Now, if we have just one Clip selected, we'll form a special menu, which lists all versions
+      if len(clipBinItems)==1:
+        versions = clipBinItems[0].items()
+        for version in versions:
+          self._versionActions+=[self.makeActionFromVersion(version)]
+          
+
+      for act in self._versionActions:
+        versionEverywhereMenu.addAction(act)
+
+        # We have a mixture of Clips, so we can't pick a Version, offer options for Prev/Next/Min/Max
+    return versionEverywhereMenu
+
+
+  # This handles events from the Project Bin View
+  def binViewEventHandler(self,event):
+
+    if not hasattr(event.sender, 'selection'):
+      # Something has gone wrong, we should only be here if raised
+      # by the Bin view which gives a selection.
+      return
+    selection = event.sender.selection()
+
+    # Return if there's no Selection. We won't add the Localise Menu.
+    if selection == None:
+      return
+
+    # Only add the Menu if Bins or Sequences are selected (this ensures menu isn't added in the Tags Pane)
+    if len(selection) > 0:
+      self._versionEverywhereMenu = self.createVersionEveryWhereMenuForView('kBin')
+      
+      # Insert the Tags menu with the Localisation Menu
+      hiero.ui.insertMenuAction(self._versionEverywhereMenu.menuAction(), event.menu)
+    return
+
+# Instantiate the Menu to get it to register itself.
+VersionAllMenu = VersionAllMenu()
